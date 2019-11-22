234314912343093e029616131cabc1a03049d5ce
Igor Couto <igor@cre8iv.click>
2019-11-22 16:36:18 +1100

    fix error with sitemap.xsl if short_open_tag is on
    Signed-off-by: Igor Couto <igor@cre8iv.click>

--- snippets/xsl.php
+++ snippets/xsl.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>'  ?>
 <xsl:stylesheet
         version="1.0"
         xmlns:sm="http://www.sitemaps.org/schemas/sitemap/0.9"
