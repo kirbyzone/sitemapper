1ee8af472268d3dd357e886271a906091ef242f3
Igor Couto <igor@cre8iv.click>
2019-11-22 16:37:20 +1100

    make ‘lang’ & ‘images’ keys in map array optional
    Signed-off-by: Igor Couto <igor@cre8iv.click>

--- snippets/xml.php
+++ snippets/xml.php
@@ -3,20 +3,32 @@
 <?= '<?xml-stylesheet type="text/xsl" href="sitemap.xsl"?>' ?>
 
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-<?php foreach ($map as $id => $page): ?>
+<?php
+foreach ($map as $id => $page):
+?>
 <url>
     <loc><?= $id ?></loc>
     <lastmod><?= $page['mod'] ?></lastmod>
-<?php foreach ($page['lang'] as $l): ?>
+<?php
+    if(array_key_exists('lang', $page)):
+        foreach ($page['lang'] as $l):
+?>
     <xhtml:link rel="alternate" hreflang="<?= $l['locale'] ?>" href="<?= $l['url'] ?>" />
 <?php
-    endforeach;
-    foreach ($page['images'] as $img): ?>
+        endforeach;
+    endif;
+    if(array_key_exists('images', $page)):
+        foreach ($page['images'] as $img): ?>
     <image:image>
         <image:loc><?= $img ?></image:loc>
     </image:image>
-<?php endforeach; ?>
+<?php
+        endforeach;
+    endif;
+?>
 </url>
-<?php endforeach; ?>
+<?php
+endforeach;
+?>
 </urlset>
 <!-- Sitemap generated using https://gitlab.com/cre8ivclick/sitemapper -->
